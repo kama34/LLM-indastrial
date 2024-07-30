54ead6c8e372c0ff9a0bb1cbba146440c3d43aa5
kama34 <kamishnikov98@gmail.com>
2024-07-30 23:26:23 +0000

    Rename from use_vllm to api_generate

--- demo.py
+++ demo.py
@@ -7,7 +7,7 @@ from typing import List, Tuple
 import json
 
 # Import the generate_theme and generate_exam_question functions from the other file
-from use_vllm import get_model, get_model_list, ExamQuestion
+from api_generate import get_model, get_model_list, ExamQuestion
 import streamlit.components.v1 as components
 
 debug = False
